a9aa5f9f58a10f0b39d921eda8d4e2fe56408a6e
Sergey Belyakov <39454357+srgblkv@users.noreply.github.com>
2021-04-11 01:54:39 +1000

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,12 +4,13 @@ require 'config.php';
 foreach ($_POST['files'] as $file_name) {
     $filename = $UPLOAD_PATH . $file_name;
 
-    if (unlink($filename)) {
-        echo 'Выбранные файлы успешно удалены.';
-    } else {
+    if (!unlink($filename)) {
         echo 'Произошла непредвиденная ошибка!';
+        break;
     }
 }
+
+echo 'Выбранные файлы успешно удалены.';
 
 
 
